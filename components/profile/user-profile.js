0870c6f2fcb8fe8a685d4a042ddc96b823785a44
Persiwal4 <72599764+Persiwal@users.noreply.github.com>
2023-06-02 14:56:09 +0200

    Create changePassword function and pass it as prop

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -3,6 +3,33 @@ import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 //import { useSession, getSession } from 'next-auth/client';
 
+async function changePassword(oldPassword, newPassword) {
+  try {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        oldPassword: oldPassword,
+        newPassword: newPassword
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (!response.ok) {
+      console.log(data.message);
+      throw new Error(data.message || 'Something went wrong');
+    }
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 function UserProfile() {
   // const [isLoading, setIsLoading] = useState(true);
 
@@ -23,7 +50,7 @@ function UserProfile() {
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePasswordHandler={changePassword} />
     </section>
   );
 }
